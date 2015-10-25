bf7e5c18c6730544f2c6ce9c62a19048e519a319
JF Roy <roy@carretechnologies.com>
2015-10-25 19:20:40 -0400

    prepare for final submission
    All code is commented
    new file for code Book, would need more clarification

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
-# GetCleanDataProject
-Getting and Cleaning Data project
+# Getting and Cleaning Data project
 
 The purpose of this project is to demonstrate your ability to collect, work with, and clean a data set. The goal is
 to prepare tidy data that can be used for later analysis. You will be graded by your peers on a series of yes/no
@@ -7,11 +6,9 @@ questions related to the project. You will be required to submit:
 
 1. a tidy data set as described below, 
 2. a link to a Github repository with your script for performing the analysis, and 
-3. a code book that describes the variables, the data, and any transformations or work that you performed to clean
-up the data called CodeBook.md.
+3. a code book that describes the variables, the data, and any transformations or work that you performed to clean up the data called CodeBook.md.
 
-You should also include a README.md in the repo with your scripts. This repo explains how all of the scripts work 
-and how they are connected.  
+You should also include a README.md in the repo with your scripts. This repo explains how all of the scripts work and how they are connected.  
 
 One of the most exciting areas in all of data science right now is wearable computing - see for example this article . Companies like Fitbit, Nike, and Jawbone Up are racing to develop the most advanced algorithms to attract new users. The data linked to from the course website represent data collected from the accelerometers from the Samsung Galaxy S smartphone. A full description is available at the site where the data was obtained: 
 
@@ -21,9 +18,22 @@ Here are the data for the project:
 
 https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip 
 
-
+## The run_analysis scripts follows these steps:
 1. Merges the training and the test sets to create one data set.
+    1. Merging dataset is performed by creating new column in the X data input for activity and subject
+    2. since file already have the same format the test and training data can be simply appended together with bind_rows()
+    
 2. Extracts only the measurements on the mean and standard deviation for each measurement. 
+    1. use the grepl to parse the features list with mean OR sts as contraint
+    2. select cols with matching regular expression, ADD activity and subject id to the list for the rest of the test
+
 3. Uses descriptive activity names to name the activities in the data set.
+    1. don't search too long, I read the forum and saw the suggestion about factor usage with labels, it works like a charm.
+
 4. Appropriately labels the data set with descriptive variable names. 
+    1. Reuse the activity liste used in (3)
+    2. use the data.frame column naming normal way to do so...
+
 5. From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+    1. write.table
+    2. Do not forget to remove header name with row.name=FALSE as written in the project description!
--- run_analysis.R
+++ run_analysis.R
@@ -3,19 +3,58 @@
 
 #---------------------------------
 # 1- Merges the training and the test sets to create one data set.
-## Data has been previously downloaded in the working directory as [working directory]/UCI HAR Dataset/
+## Data has been previously downloaded in the working directory in ./UCI HAR Dataset/
+#oad everything in data frame table
+X_train <- read.table("UCI HAR Dataset/train/X_train.txt", stringsAsFactors=FALSE)
+Y_train <- read.table("UCI HAR Dataset/train/Y_train.txt", stringsAsFactors=FALSE)
+subject_train <- read.table("UCI HAR Dataset/train/subject_train.txt", stringsAsFactors=FALSE)
+X_test <- read.table("UCI HAR Dataset/test/X_test.txt", stringsAsFactors=FALSE)
+Y_test <- read.table("UCI HAR Dataset/test/Y_test.txt", stringsAsFactors=FALSE)
+subject_test <- read.table("UCI HAR Dataset/test/subject_test.txt", stringsAsFactors=FALSE)
+
+library(dplyr)
+#identify test source of data
+d <- mutate(X_test, status = "test")
+#combine data in same table instead of multiple source
+d$activity <- Y_test$V1
+d$subject <- subject_test$V1
+
+#identify train source of data
+dd <- mutate(X_train, status = "train")
+#combine data in same table instead of multiple source
+dd$activity <- Y_train$V1
+dd$subject <- subject_train$V1
+
+#here is the merged result
+ddd <- bind_rows(d,dd)
 
 
 #---------------------------------
-# 2- Extracts only the measurements on the mean and standard deviation for each measurement. 
+# 2- Extracts only the measurements on the mean and standard deviation for each measurement.
+#read data
+features <- read.table("UCI HAR Dataset/features.txt", stringsAsFactors=FALSE)
+#find string with mean OR std
+flist <- features[grepl("mean|std", features$V2),]
+#then extract corresponding column including activity and subject
+subdata <- ddd[,c(sapply(flist$V1, function(x) {sprintf("V%d",x)}), "activity", "subject")]
 
 #---------------------------------
 # 3- Uses descriptive activity names to name the activities in the data set
+activities <-  read.table("UCI HAR Dataset/activity_labels.txt",stringsAsFactors=FALSE)
+#using factor, convert activity ids to labels
+subdata$activity <- as.factor(activities$V2[subdata$activity])
+#subdata$activity <- factor(subdata$activity, labels = activities$V2)
 
 #---------------------------------
-# 4- Appropriately labels the data set with descriptive variable names. 
+# 4- Appropriately labels the data set with descriptive variable names.
+colnames(subdata) <- c(sapply(flist$V2, function(x) {sprintf("%s",x)}), "activity", "subject")
 
 #---------------------------------
 # 5- From the data set in step 4, creates a second, independent tidy data set with the average
 # of each variable for each activity and each subject.
+# Please upload the tidy data set created in step 5 of the instructions. 
+# Please upload your data set as a txt file created with write.table() using row.name=FALSE
+gdata <- group_by(subdata, activity, subject)
+tidydata <- summarise_each(gdata, funs(mean))
+write.table(tidydata, "UCI HAR Dataset/NewDataset.txt", row.name=FALSE)
 
